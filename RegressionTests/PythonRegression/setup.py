491dec9d758716361a998077ee913d234cafa2c9
DyrellC <dyrell.chapman@iota.org>
2018-06-28 14:11:59 -0600

    Removed venv, updated setup.py with dependencies

---
+++ RegressionTests/PythonRegression/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='IRI_PyRegression',
+    description='Regression testing for IRI using Aloe',
+    url='https://github.com/DyrellC/iri_regression_tests',
+    author='DyrellC',
+    packages=['util','features'],
+    install_requires=[
+        'pyota',
+        'aloe'     
+    ]       
+    )
