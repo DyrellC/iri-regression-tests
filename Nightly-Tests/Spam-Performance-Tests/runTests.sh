33c9b0f9567ca0d9cf391c97dfd2c225873e5187
DyrellC <dyrell.chapman@iota.org>
2019-05-03 17:32:21 -0600

    Spam Performance Tests init commit

---
+++ Nightly-Tests/Spam-Performance-Tests/runTests.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo
+    echo "Exit called by user"
+    kill -9 ${IRI}
+    exit
+}
+
+echo "Downloading apt requirments "
+sed 's/#.*//' requirements.txt | xargs sudo apt-get install -y
+
+UUID="$(uuidgen)"
+K8S_NAMESPACE=$(kubectl config get-contexts $(kubectl config current-context) | tail -n+2 | awk '{print $5}')
+base_dir=$(pwd)
+
+
+if [[ ! -d ./iri/ ]]; then
+    echo "Downloading IRI"
+    git clone https://github.com/iotaledger/iri
+fi
+
+cd iri 
+git fetch
+git pull
+mvn clean compile | mvn package -DskipTests
+cd ../
+echo "IRI installed"
+
+echo "Setting up Jmeter"
+if [[ ! -d ./apache-jmeter-5.1.1/ ]]; then
+    curl -LO https://www-eu.apache.org/dist//jmeter/binaries/apache-jmeter-5.1.1.tgz
+    tar -xf apache-jmeter-5.1.1.tgz
+    echo "" >> ./apache-jmeter-5.1.1/bin/user.properties
+    echo "jmeter.save.saveservice.output_format=xml" >> ./apache-jmeter-5.1.1/bin/user.properties
+    echo "jmeter.save.saveservice.response_data=true" >> ./apache-jmeter-5.1.1/bin/user.properties
+fi
+
+if [ -f ./db-* ]; then
+    rm db-*
+fi
+
+echo "Downloading db"
+curl -LO https://s3.eu-central-1.amazonaws.com/iotaledger-dbfiles/dev/SyncTestSynced.tar.gz
+
+echo "Unpacking db"
+tar -xvf SyncTestSynced.tar.gz
+mv ./testnetdb ./iri/target/
+cd ./iri/target/
+IRI_TARGET=$(pwd)
+
+cd ${base_dir}
+echo "Installing python requirements"
+virtualenv venv
+source ./venv/bin/activate
+cd ../
+pip install --upgrade pip
+pip install -e .
+cd ${base_dir}
+
+cd ${IRI_TARGET}
+echo "Starting IRI"
+nohup java -jar iri-1.* -p 14265 -u 14600 --testnet true --testnet-no-coo-validation true --testnet-coordinator EFPNKGPCBXXXLIBYFGIGYBYTFFPIOQVNNVVWTTIYZO9NFREQGVGDQQHUUQ9CLWAEMXVDFSSMOTGAHVIBH --mwm 1 --milestone-start 0 --remote-limit-api "" --remote true &> nodelog.out &
+IRI=$!
+
+# Give node time to start up
+sleep 15
+cd ${base_dir}
+
+NODE="NodeA"
+HOST="localhost"
+PORT="14265"
+
+bash ./startJmeter.sh ${NODE} ${HOST} ${PORT} ${UUID} ${K8S_NAMESPACE}
+
+deactivate
---
+++ Nightly-Tests/Spam-Performance-Tests/startJmeter.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+NODE=$1
+HOST=$2
+PORT=$3
+UUID=$4
+NAMESPACE=$5
+
+DATE=$(date +'%m-%d-%Y')
+
+CWD=$(pwd)
+BASE_DIRECTORY="$CWD/Output/"
+DATE_DIRECTORY="$BASE_DIRECTORY$DATE/"
+LOG_DIRECTORY="$DATE_DIRECTORY$NODE"
+echo ${LOG_DIRECTORY}
+
+if ! [[ -d ${BASE_DIRECTORY} ]];then
+    mkdir ${BASE_DIRECTORY}
+fi
+
+if ! [[ -d ${DATE_DIRECTORY} ]];then
+    mkdir ${DATE_DIRECTORY}
+fi
+
+if [[ -d ${LOG_DIRECTORY} ]]; then
+    rm -r ${LOG_DIRECTORY}
+    mkdir ${LOG_DIRECTORY}
+else
+    mkdir ${LOG_DIRECTORY}
+fi
+
+cd ${LOG_DIRECTORY}
+
+for FILE in ${CWD}/apiData/*.txt; do
+    FILENAME=$(basename ${FILE} .txt)
+
+    mkdir ${FILENAME}
+    cd ${FILENAME}
+    
+    LOG_FILE="$NODE.jtl"
+    RUNLOG_FILE="runlog.log"
+    OUTPUT="testLog.out"
+
+    echo "Starting Single Thread test for $FILENAME on $NODE"
+
+    mkdir "./SingleThread/"
+    cd "./SingleThread/"
+    
+    SINGLE_THREAD=$(pwd)
+    cd ${CWD}    
+    nohup python ./runScan.py -o ./SingleThreadScanLogs/ -i 0.5 -n 360 -c ${FILENAME} &> SingleThreadScanLog.out &    
+    cd ${SINGLE_THREAD}
+    
+    nohup bash ${CWD}/apache-jmeter-5.1.1/bin/jmeter.sh -n -t ${CWD}/iriRequests.jmx -l ${LOG_FILE} \
+    -j ${RUNLOG_FILE} -Jhost ${HOST} -Jport ${PORT} -Jfile ${FILE} -JnumThreads 1 \
+    -JnumLoops 100 &> "${FILENAME}-jmeter-single-out.log" &
+
+    sleep 200
+
+    echo "Starting MultiThread test for $FILENAME on $NODE"
+
+    cd ../
+    mkdir "./MultiThread/"
+    cd "./MultiThread/"
+    
+    MULTI_THREAD=$(pwd)
+    cd ${CWD}
+    nohup python ./runScan.py -o ./MultiThreadScanLogs/ -i 0.5 -n 360 -c ${FILENAME} &> MultiThreadScanLog.out &    
+    cd ${MULTI_THREAD}
+    
+    nohup bash ${CWD}/apache-jmeter-5.1.1/bin/jmeter.sh -n -t ${CWD}/iriRequests.jmx -l ${LOG_FILE} \
+    -j ${RUNLOG_FILE} -Jhost ${HOST} -Jport ${PORT} -Jfile ${FILE} -JnumThreads 100 \
+    -JnumLoops 1 &> "${FILENAME}-jmeter-multi-out.log" &
+
+    sleep 200 
+
+    cd ${LOG_DIRECTORY}
+done 
+
